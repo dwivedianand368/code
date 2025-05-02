ba53dbdfdd2258eebef88a48e0d31f1147d809d7
dwivedianand368 <dwivedianand368@gmail.com>
2025-05-03 00:14:43 +0530

    okk

---
+++ js/day2.js
@@ -0,0 +1,25 @@
+function and(){
+    console.log("Welcome to the js")
+
+
+}
+and()
+
+function add (){
+
+    var a = 10;
+    var b = 20;
+    console.log("Sum of a + b is " + (a+b));
+}
+add()
+
+//alert("This is alert");
+//window.confirm("This is alert");
+
+// var ans = confirm("Are you ook")
+// console.log(ans);
+
+//BOM => inteaction between browser and window
+
+var ans = prompt("Do you know my name");
+console.log(ans);
