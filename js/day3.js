0a9c9b3caeb5e0ae450dfedfd36272a1f3bf91b6
dwivedianand368 <dwivedianand368@gmail.com>
2025-05-06 14:29:13 +0530

    okk

---
+++ js/day3.js
@@ -0,0 +1,49 @@
+// function okk(){
+//     // parseInt() , parseFloat(),  Number()
+
+
+//     var  a1 = Number(prompt("Enter no. 1")) //"3" => 3
+//     var a2 = Number(prompt("Enter no. 2")) //"5" => 5
+//     alert(a1+a2)                         //"3"+"5" => 35
+// }
+
+// okk();
+
+
+// function Anand(){
+//     var age = Number(prompt("Enter your age"))
+//     if(age>=18){
+//         alert("you can vote")
+//     }
+//     else{
+//         alert("not to vode")
+//     }
+// }
+
+// Anand();
+
+// function Number(){
+//     for(var i = 1; i<=10; i++){
+//         console.log(i)
+//     }
+// }
+// Number()
+
+
+// var ans = confirm("Do you like my website")
+
+// if(ans){
+//     alert("Thanks")
+// }
+// else{
+//     alert("Sorry")
+// }
+
+
+var age = Number(prompt("Enter your age"));
+var age =  (age >= 18)? "you can vote ": "you can't vote";
+alert(age);
+
+
+
+
