b267353195a24cd6a23cc26cb9af57282a84aeb2
dwivedianand368 <dwivedianand368@gmail.com>
2025-05-08 00:51:00 +0530

    ook

--- js/day4.js
+++ js/day4.js
@@ -15,4 +15,11 @@
 
  //var            let             const
 
- //let we can not Recleared    
+ //let we can not Recleared  
+ function one(){
+    var a = 5;
+    var b = 6;
+    var c = a+b;
+    console.log(c);
+ }  
+ one()
