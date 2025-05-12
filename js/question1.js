6a5594bb818b0565b1292f51e7cf64e5dee4cc15
dwivedianand368 <dwivedianand368@gmail.com>
2025-05-12 14:42:33 +0530

    ook

---
+++ js/question1.js
@@ -0,0 +1,61 @@
+// function length(){
+//     let a = "Anand";
+//     console.log(a.length);
+// } 
+// length();
+
+
+
+// function slice(){
+//     let a = "ThisIsTheBestFunction";
+//     console.log(a.slice(9,13));
+// }
+// slice();
+
+
+
+// function concat(){
+//     let a = "Anand";
+//     let b = "Dwivedi";
+//     console.log(a.concat(b))
+
+// }
+// concat();
+
+
+// function substring(){
+//     let a =  "ThisIsTheBestFunction";
+//     console.log(a.substring(9,13));
+// }
+// substring();
+
+//---------------------------------problem on this code----------------------------
+// function replace(){
+//     let a = "Place";
+//     let b = "Replace";
+//     console.log(a.replace(b));
+// }
+// replace();
+
+
+
+
+// function toUpperCase(){
+//     let str = "anand";
+//     console.log(str.toUpperCase());
+// }
+// toUpperCase();
+
+
+// function toLowerCase(){
+//     let str = "ANAND";
+//     console.log(str.toLowerCase());
+// }
+// toLowerCase();
+
+
+// function trim(){
+//     let str = "       Boss       ";
+//     console.log(str.trim());
+// }
+// trim();
