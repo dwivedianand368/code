316fc3c544ceabe731b5df1edc84384d5c739126
dwivedianand368 <dwivedianand368@gmail.com>
2025-05-15 19:14:52 +0530

    ook

--- js/day7.js
+++ js/day7.js
@@ -13,7 +13,21 @@
 // console.log(Fruits)
 
 
-let arr1 = [1,2,3,4,]
-let arr2 = [5,6,7,8,]
-let arr3 = [7,8,9,10]
-console.log(arr1.concat(arr2,arr2))
+// let arr1 = [1,2,3,4,]
+// let arr2 = [5,6,7,8,]
+// let arr3 = [7,8,9,10]
+// console.log(arr1.concat(arr2,arr2))
+
+// let arr1 = [1,2,3,4,5]
+// console.log(arr1.reverse()) 
+
+
+// array to string => join();
+// String to Array => split(); humse saperater magta hai space ye - kuch bhi 
+
+
+
+let num = 758714
+console.log(Number(num.toString().split('').reverse().join('')))
+
+
