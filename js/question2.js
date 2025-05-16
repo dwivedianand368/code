91ed196cf335dcf349c88216a525916834a86d9d
dwivedianand368 <dwivedianand368@gmail.com>
2025-05-17 01:56:32 +0530

    ook

--- js/question2.js
+++ js/question2.js
@@ -58,15 +58,58 @@
 // Reverse an Array
 // Reverse the array [10, 20, 30, 40, 50] without using the reverse() method.
 
-let arr = [10,20,30,40,50,];
-let start = 0;
-let end = arr.length-1;
-while(start<end){
-    let temp = arr[start]
-    arr[start] = arr[end]
-    arr[end] = temp
-    start++
-    end--
-}
-console.log("reversed arry " ,arr)
+// let arr = [10,20,30,40,50,];
+// let start = 0;
+// let end = arr.length-1;
+// while(start<end){
+//     let temp = arr[start]
+//     arr[start] = arr[end]
+//     arr[end] = temp
+//     start++
+//     end--
+// }
+// console.log("reversed arry " ,arr)
+
+
+//=============================second method of reverse=========================================================================
+
+
+// let arr = [10,20,30,40,50]
+// let reversed = []
+// for(let i=arr.length-1; i>=0 ; i--)
+// {
+//     reversed.push(arr[i])
+// }
+// console.log("reversed:" ,reversed)
+// =====================================================================================================================================//
+
+// Check if an Element Exists
+// Write a program to check if a glven number (e.g., 9) exists in the array [3, 5, 7, 9, 11].
+
+// let arr = [3,5,7,9,11]
+// let target = 9
+// let b = false;
+// for(let i = 0; i <=arr.length-1; i++){
+//     if(arr[i]==target){
+//         b = true;
+//         break
+//     }
+// }
+// console.log(b)
+
+
+// ======================================================================================================================================//
+
+//Merge [1, 2, 3] and [4, 5, 6] into a new array without using .concat().
+
+// let arr1 = [1,2,3,4]
+// let arr2 = [5,6,7,8]
+// let merged = []
+// for(let i=0; i<arr1.length;i++){
+//     merged.push(arr1[i])
+// }
+// for(let i = 0; i<arr2.length;i++){
+//     merged.push(arr2[i])
+// }
+// console.log(merged)
 
