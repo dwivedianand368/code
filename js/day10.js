902ccb74b57fb979b8d9d45471ddfe45e21edbe7
dwivedianand368 <dwivedianand368@gmail.com>
2025-05-20 20:59:24 +0530

    ook

---
+++ js/day10.js
@@ -0,0 +1,48 @@
+// let welcome=(myname)=>{
+//     console.log("Hello" + myname);
+// }
+// welcome("Anand")
+
+
+
+// let add =(a,b)=>{
+//     console.log(a+b)
+// }
+// add(1,5)
+
+
+// let ana=(a,b)=>{
+//     return a+b;
+// }
+// let result =ana(4,8);
+// console.log(result)
+
+
+
+
+// let marks=(math,physics,hindi)=>{
+//     let percent = (math+physics+hindi)/3
+//     return percent
+    
+ 
+// }
+// let result = marks(78,79,86);
+// console.log(result)
+
+
+// let si=(p,r,t)=>{
+//     let total = p*r*t*0.01 
+//     return total
+// }
+// let result = si(1000,6,6);
+// console.log(result)
+
+// console.log(ter)
+
+let num =(a)=>{
+    let ter = (a>0)?"+":"-";
+    return ter
+}
+let result= num(a)
+console.log(result)
+
