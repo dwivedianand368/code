285a1ccd679563edae467cb8f04a64b6b988002a
dwivedianand368 <dwivedianand368@gmail.com>
2025-05-23 21:39:42 +0530

    ook

---
+++ js/DOM12.js
@@ -0,0 +1,36 @@
+
+
+let red=()=>{
+    let myname = document.querySelector("#box")
+    myname.style.background = "red"
+    let Btn1 = document.querySelector(".btn1")
+
+    Btn1.style.backgroundColor="red"
+
+    myname.style.backgroundImage="url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzbTezYange_PJP3KLcuf3kr6q_5uQtRt-IgMo9p8pvmC1gWMGF9-YcaAEGk4m7l95hzZkHfCcJQ5hMQrBU43CHg')"
+}
+
+let yellow=()=>{
+    let myname = document.querySelector("#box")
+    myname.style.background = "yellow"
+    let Btn2 = document.querySelector(".btn2")
+
+        Btn2.style.backgroundColor="yellow"
+
+
+}
+
+let blue=()=>{
+    let myname = document.querySelector("#box")
+    myname.style.background = "blue"
+    let Btn3 = document.querySelector(".btn3")
+
+        Btn3.style.backgroundColor="blue"
+
+}
+
+
+
+
+
+
