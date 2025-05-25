200c864ae5001c18d87739bd24ce06c10fda40be
dwivedianand368 <dwivedianand368@gmail.com>
2025-05-26 01:51:36 +0530

    ook

---
+++ js/Day13.js
@@ -0,0 +1,14 @@
+let btn1=()=>{
+    let Image = document.querySelector("#pic2")
+    Image.src="pic2.webp"
+}
+
+let btn2=()=>{
+    let a = document.querySelector("#pic2")
+    a.style.display="none"
+}
+
+let btn3=()=>{
+    let b = document.querySelector("#pic2")
+    b.style.display="flex"
+}
