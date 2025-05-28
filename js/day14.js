2182da671cc772dd8b77fd116c15e5eb5347c52b
dwivedianand368 <dwivedianand368@gmail.com>
2025-05-28 21:51:58 +0530

    ook

---
+++ js/day14.js
@@ -0,0 +1,19 @@
+// let showw =()=>{
+//     let inp = document.querySelector("#inptext").value    //value is liye likhe hai q ki jo value hai use lena hai
+
+//     let show=document.querySelector("#show")
+    
+//     show.innerHTML=inp
+// }
+
+
+
+
+let answer=()=>{
+    let num1 = document.querySelector("#inptext1").value
+    let num2 = document.querySelector("#inptext2").value
+
+    let one=document.querySelector("#one")
+    one.innerHTML=Number(inp1)+ Number(inp2)
+
+}
