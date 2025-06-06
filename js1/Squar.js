2ec3bcdffc0bf4ea55373614a6e21a7e65c83c63
dwivedianand368 <dwivedianand368@gmail.com>
2025-06-06 23:14:00 +0530

    ook

---
+++ js1/Squar.js
@@ -0,0 +1,15 @@
+
+let blue=()=>{
+    let myname = document.querySelector("#box")
+    myname.style.background = "blue"
+
+
+}
+let yellow=()=>{
+    let myname = document.querySelector("#box")
+    myname.style.background = "yellow"
+    
+
+
+
+}
