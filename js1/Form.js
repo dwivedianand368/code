0893c0f5bf511627f8721505099a7778285e9b39
dwivedianand368 <dwivedianand368@gmail.com>
2025-06-12 19:20:12 +0530

    ook

--- js1/Form.js
+++ js1/Form.js
@@ -1,9 +1,9 @@
 let formValidate=()=>{
     let inpname = document.querySelector("#name").value
-    let inpnum = document.querySelector("#name").value
-    let inpemail = document.querySelector("#name").value
-    let inppass = document.querySelector("#name").value
-    let incpass = document.querySelector("#name").value
+    let inpnum = document.querySelector("#num").value
+    let inpemail = document.querySelector("#email").value
+    let inppass = document.querySelector("#pass").value
+    let incpass = document.querySelector("#cpass").value
 
 
     let errname=document.querySelector("#errname")
@@ -12,27 +12,44 @@ let formValidate=()=>{
     let errpass=document.querySelector("#errpass")
     let errcpass=document.querySelector("#errcpass")
 
-    if(inpname==""){
-        let namebox=document.querySelector("#name")
+    if(inpname=="")
+    {
+        namebox=document.querySelector("#name")
         namebox.style.border="1px solid red"
         errname.innerHTML="Please fill this form"
         errname.style.color="red"
         return false
     }
-    else if(inpnum==""){
+    else if(inpnum=="")
+    {
         errnum.innerHTML="please enter number"
         return false
     }
-    else if(isNaN(inpnum)){    //if alphabtes return true
+    else if(isNaN(inpnum))
+    {    //if alphabtes return true
         errnum.innerHTML="Please enter number only"
         return false;
     }
-    else if(inpnum.length != 10){
+    else if(inpnum.length != 10)
+    {
         errnum.innerHTML="Plese Enter 10 digits only"
         return false
     }
-    else if((inpemail.includes("@") && inpemail.includes(".com"))){
+    else if(!(inpemail.includes("@") && inpemail.includes(".com"))){
         erremail.innerHTML="please enter valid email"
         return false
+    }
+    else if (pass != cpass){
+        errcpass.innerHTML = "Please inter same password"
+    }
+    else if (!(pass.match(/[0-9]]/) &&
+            pass.match(/[!@#$%^&*()]/)&&
+            
+            Pass.match(/[a-z]/)&&
+            Pass.match(/[A-Z]/)
+        )){
+
+            errpass.innerHTML = "please enter strong password"
+
     }
 }
